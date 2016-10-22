5d89862194a4f4d22d668be838551d1aea64a548
Josh <josh@joshcruz.net>
2016-10-21 19:39:55 -0700

    bug fix: logout after redirect

--- photography/public/logout.php
+++ photography/public/logout.php
@@ -1,6 +1,5 @@
 <?php require_once("../includes/session.php"); ?>
 <?php require_once("../includes/functions.php"); ?>
-
 <?php
 	// v1: simple logout
 	// session_start();
@@ -8,7 +7,6 @@
 	$_SESSION["username"] = null;
 	redirect_to("login.php");
 ?>
-
 <?php
 	// v2: destroy session
 	// assumes nothing else in session to keep
